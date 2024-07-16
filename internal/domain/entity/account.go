3485c22e6463a236f2900593cbdf65c65f05d857
Sergei Hilton <sergei.hilton@gmail.com>
2024-07-16 17:30:17 +0500

    feat: implement all account operations

--- internal/domain/entity/account.go
+++ internal/domain/entity/account.go
@@ -1,6 +1,11 @@
 package entity
 
-import "sync"
+import (
+	"errors"
+	"sync"
+)
+
+var LockError = errors.New("another operation is already in progress for this account")
 
 type Account struct {
 	id      string
@@ -16,18 +21,34 @@ func NewAccount(id string) *Account {
 }
 
 func (a *Account) Deposit(amount float64) error {
-	//TODO implement me
-	panic("implement me")
+	isLock := a.mu.TryLock()
+	if !isLock {
+		return LockError
+	}
+	a.balance += amount
+	a.mu.Unlock()
+	return nil
 }
 
 func (a *Account) Withdraw(amount float64) error {
-	//TODO implement me
-	panic("implement me")
+	isLock := a.mu.TryLock()
+	if !isLock {
+		return LockError
+	}
+
+	if amount > a.balance {
+		return errors.New("insufficient funds")
+	}
+
+	a.balance -= amount
+	a.mu.Unlock()
+	return nil
 }
 
 func (a *Account) GetBalance() float64 {
-	//TODO implement me
-	panic("implement me")
+	a.mu.Lock()
+	defer a.mu.Unlock()
+	return a.balance
 }
 
 func (a *Account) Id() string {
