e89b8ba1990ccca8bcb0c69a3b5cc3f139c7e4f7
Sergei Hilton <sergei.hilton@gmail.com>
2024-07-16 16:02:00 +0500

    chore: implement the AccountRepository interface

---
+++ internal/infrastructure/repository/memory/account.go
@@ -0,0 +1,25 @@
+package memory
+
+import "s3ai-testtask/internal/domain/interfaces"
+
+type AccountRepository struct {
+}
+
+func NewAccountRepository() interfaces.AccountRepository {
+	return &AccountRepository{}
+}
+
+func (b *AccountRepository) GetAccount(id string) (interfaces.BankAccount, bool) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (b *AccountRepository) CreateAccount() (interfaces.BankAccount, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (b *AccountRepository) SaveAccount(account interfaces.BankAccount) {
+	//TODO implement me
+	panic("implement me")
+}
